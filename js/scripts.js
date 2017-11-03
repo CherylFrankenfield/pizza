6f5bf419fc147eee96789320d34ae6db44d12fe3
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2017-11-03 10:59:19 -0700

    Pass spec 3 and pass spec 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 // Business Logic here
-function Pizza(size, topping) {
-  this.size = size;
-  this.topping = topping;
-}
-
-Pizza.prototype.yourPizza = function() {
-  return this.size + ", " + this.topping;
-}
+// function Pizza(size, topping) {
+//   this.size = size;
+//   this.topping = topping;
+// }
+//
+// Pizza.prototype.yourPizza = function() {
+//   return this.size + ", " + this.topping;
+// }
 
 
 
@@ -14,10 +14,25 @@ Pizza.prototype.yourPizza = function() {
 $(document).ready(function() {
   $("form#select-order").submit(function(event) {
     event.preventDefault();
+    var pizzaSize = [];
+    var pizzaTopping = [];
+    $("input:checkbox[name=size]:checked").each(function(){
+      var sizeSelection = $(this).val();
+      pizzaSize.push(sizeSelection);
+    });
+    $("input:checkbox[name=topping]:checked").each(function(){
+      var toppingSelection = $(this).val();
+      pizzaTopping.push(toppingSelection);
+    });
 
-    var pizzaSize = $("input#size").val();
-    var pizzaTopping = $("input#topping").val();
-    $("ul#your-pizza").append("<li>" + pizzaSize + "</span></li>");
+    $("p#pizza-ordered").show();
+    $("#your-pizza").append(pizzaSize + "<br>", pizzaTopping + "<br>");
+    // $("ul#your-pizza").append("<li>" + pizzaSize + pizzaTopping + "</span></li>");
+
+    // var pizzaSize = $("input#size").val();
+    // var pizzaTopping = $("input#topping").val();
+
+
 
     // var newPizzaOrder = new Pizza(pizzaSize, pizzaTopping)
     //
