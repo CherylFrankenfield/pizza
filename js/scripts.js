e7aad0fb0ed11f89fa35d1827ef8eb28e873f9c4
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2017-11-06 21:26:54 -0800

    Assign dollar values to topping form

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,64 @@
-Business Logic here
+// Business Logic here
 function Pizza(size, topping) {
   this.size = size;
   this.topping = topping;
   this.price;
-  this.yourSizePrice();
-  this.yourToppingPrice();
-  this.yourCost();
+  this.yourToppingPrice = [];
 }
 
-Pizza.prototype.yourSizePrice = function() {
-  if (this.size === "Puny small") {
-    this.price = 8.00;
-  } else if (this.size === "Boring medium") {
-    this.price = 12.00;
-  } else if (this.size === "Go-big-or-go-home") {
-    this.price = 14.00;
-  }
-  return this.price;
+var add = function(a,b) {
+  return a + b;
 }
+// Pizza.prototype.yourSizePrice = function() {
+//   if (this.size === "Puny small") {
+//     this.price = 8.00;
+//   } else if (this.size === "Boring medium") {
+//     this.price = 12.00;
+//   } else if (this.size === "Go-big-or-go-home") {
+//     this.price = 14.00;
+//   }
+//   return this.price;
+// }
 
-Pizza.prototype.yourToppingPrice = function() {
-  for (index = 0; index <= pizzaTopping; index++) {
-    if (this.topping === "extra cheese") {
-      this.price = .50;
-    } else if (this.topping === "mushroom") {
-      this.price = .50;
-    } else if (this.topping === "pepperoni") {
-      this.price = 1.00;
-    } else if (this.topping === "pineapple") {
-      this.price = 1.00;
-    }
-    return this.price;
-  }
-}
+// Pizza.prototype.yourToppingPrice = function() {
+//   for (index = 0; index <= pizzaTopping; index++) {
+//     if (this.topping === ".50") {
+//       this.topping = .50;
+//     } else if (this.topping === ".75") {
+//       this.price = .75;
+//       yourToppingPrice.push(this.price);
+//     } else if (this.topping === "1.25") {
+//       this.price = 1.25;
+//       yourToppingPrice.push(this.price);
+//     } else if (this.topping === "1.00") {
+//       this.price = 1.00;
+//       yourToppingPrice.push(this.price);
+//     }
+//     return add(yourToppingPrice);
+//   }
+// }
+
+
+var pizzaSize;
+var pizzaTopping = [];
+var newPizzaOrder = new Pizza(pizzaSize, pizzaTopping)
+var costOfTopping = pizzaTopping.reduce(add, 0);
 
 Pizza.prototype.yourCost = function() {
-  this.yourSizePrice += this.yourToppingPrice;
-  return this.yourCost;
+  pizzaSize += costOfTopping;
 }
-
-var pizzaSize = " ";
-var pizzaTopping = [];
-
-var newPizzaOrder = new Pizza(pizzaSize, pizzaTopping)
-
 // Interface Logic here
 $(document).ready(function() {
   $("form#select-order").submit(function(event) {
     event.preventDefault();
-    $("select#select-size").each(function(){
-      var sizeSelection = $(this).val();
-      pizzaSize.push(sizeSelection);
-    });
-    $("input:checkbox[name=topping]:checked").each(function(){
+    var pizzaSize = $("select#select-size").val();
+    $("input:checkbox[name=topping]:checked").each(function() {
       var toppingSelection = $(this).val();
       pizzaTopping.push(toppingSelection);
     });
     $(".summary").show();
     $(".total").show();
-    //Do I need to call the function?
-    // newPizzaOrder.yourCost(this.yourSizePrice, this.yourToppingPrice);
+
     $("#your-total").append("<br>" + newPizzaOrder.yourCost() + "</span></br>");
 //Shows your order summary.
     $("#your-pizza").append("</br>" + pizzaSize + "</br> with: " , pizzaTopping + "<br>");
