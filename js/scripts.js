345133ec62c53e1a93dee80f8d37eec391f15497
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2017-11-08 22:16:40 -0800

    Spec 5 passes and total displays

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,62 @@
 // Business Logic here
-function Pizza(pizzaName, pizzaToppings, pizzaSizeTotal, pizzaToppingTotal) {
-  this.name = name;
+function Pizza(toppings, sizeTotal, toppingTotal) {
   this.toppings = toppings;
   this.sizeTotal = sizeTotal;
   this.toppingTotal = toppingTotal;
   this.yourCost();
-  this.yourOrder();
+  this.theName();
 }
 
-Pizza.prototype.name = function() {
-  if (sizeTotal === "8.00") {
-    name = "Puny small";
-  } else if (sizeTotal === "12.00") {
-    name = "Boring medium";
-  } else if (sizeTotal === "14.00") {
-    name = "Go-big-or-go-home large";
+Pizza.prototype.theName = function(sizeTotal) {
+  if (sizeTotal === 8) {
+    return "Puny small";
+  } else if (sizeTotal === 12) {
+    return "Boring medium";
+  } else if (sizeTotal === 14) {
+    return "Go-big-or-go-home large";
   }
-  return name
 }
 
-var add = function(a,b) {
-  return a + b;
+Pizza.prototype = function() {
+    if ("topping" === ".50") {
+      yourOrderToppingSummary = "Extra cheese";
+      toppingSummary.push("Extra cheese");
+    } else if ("topping" === ".75") {
+      yourOrderToppingSummary = "Mushroom";
+      toppingSummary.push("Mushroom");
+    } else if ("topping" === "1.25") {
+      yourOrderToppingSummary = "Pepperoni";
+      toppingSummary.push("Pepperoni");
+    } else if ("topping" === "1.00") {
+      yourOrderToppingSummary = "pineapple";
+      toppingSummary.push("pineapple");
+    }
 }
 
-Pizza.prototype.yourCost = function() {
-  sizeTotal += toppingTotal;
-}
-
-Pizza.prototype.yourOrder = function() {
-  name += toppings;
+Pizza.prototype.yourCost = function(sizeTotal, toppingTotal) {
+  return sizeTotal + toppingTotal
 }
 
 // Interface Logic here
 $(document).ready(function() {
   $("form#select-order").submit(function(event) {
     event.preventDefault();
-    var pizzaToppingTotal = [];
+    //function w/ each loop to grab all toppings selected and add them.
+    var pizzaToppingTotal = 0;
     var pizzaSizeTotal = parseFloat($("select#select-size").val());
     $("input:checkbox[name=topping]:checked").each(function() {
       var toppingSelection = parseFloat($(this).val());
-      pizzaToppingTotal.push(toppingSelection);
-      // console.log(pizzaSizeTotal);
-      // console.log(pizzaToppingTotal);
+      pizzaToppingTotal = pizzaToppingTotal + toppingSelection;
     });
-    name();
-    console.log(name());
-    var newPizzaOrder = new Pizza(pizzaName, pizzaToppings, pizzaSizeTotal, pizzaToppingTotal)
-    Pizza.topping = pizzaToppingTotal.reduce(add, 0);
-    console.log(Pizza.topping);
-    Pizza.price = newPizzaOrder.yourCost();
-    console.log(Pizza.price);
+
+    toppings = " ";
+    var newPizzaOrder = new Pizza(pizzaToppings, pizzaSizeTotal, pizzaToppingTotal)
+    newPizzaOrder.theName();
 
     //Shows your order summary.
     $(".summary").show();
     $(".total").show();
-    $("#your-total").append("<br>" + newPizzaOrder.price + "</span></br>");
-    $("#your-pizza").append("</br>" + Pizza.name + "</br> with: " , + Pizza.toppings + "<br>");
+    $("#your-total").append("<br>" + newPizzaOrder.yourCost(pizzaSizeTotal, pizzaToppingTotal) + "</span></br>");
+    $("#your-pizza").append("</br>" + newPizzaOrder.theName(pizzaSizeTotal) + "</br> with: " , + newPizzaOrder.toppingTotal + "<br>");
   });
 });
