f4107b2a54f9bdfcd91a5d64071fe05a303d555a
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2017-11-08 20:29:34 -0800

    Add name and yourOrder prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,60 @@
 // Business Logic here
-function Pizza(size, topping) {
-  this.size = size;
-  this.topping = topping;
-  this.price;
+function Pizza(pizzaName, pizzaToppings, pizzaSizeTotal, pizzaToppingTotal) {
+  this.name = name;
+  this.toppings = toppings;
+  this.sizeTotal = sizeTotal;
+  this.toppingTotal = toppingTotal;
   this.yourCost();
+  this.yourOrder();
+}
+
+Pizza.prototype.name = function() {
+  if (sizeTotal === "8.00") {
+    name = "Puny small";
+  } else if (sizeTotal === "12.00") {
+    name = "Boring medium";
+  } else if (sizeTotal === "14.00") {
+    name = "Go-big-or-go-home large";
+  }
+  return name
 }
 
 var add = function(a,b) {
   return a + b;
 }
 
-// var pizzaSize;
-// var costOfTopping;
-// var newPizzaOrder;
-// var toppingSummary = [];
-
 Pizza.prototype.yourCost = function() {
-  this.size += this.topping;
+  sizeTotal += toppingTotal;
+}
+
+Pizza.prototype.yourOrder = function() {
+  name += toppings;
 }
 
 // Interface Logic here
 $(document).ready(function() {
   $("form#select-order").submit(function(event) {
     event.preventDefault();
-    var pizzaTopping = [];
-    var pizzaSize = parseFloat($("select#select-size").val());
+    var pizzaToppingTotal = [];
+    var pizzaSizeTotal = parseFloat($("select#select-size").val());
     $("input:checkbox[name=topping]:checked").each(function() {
       var toppingSelection = parseFloat($(this).val());
-      pizzaTopping.push(toppingSelection);
-      console.log(pizzaTopping);
+      pizzaToppingTotal.push(toppingSelection);
+      // console.log(pizzaSizeTotal);
+      // console.log(pizzaToppingTotal);
     });
-
-    var newPizzaOrder = new Pizza(pizzaSize, pizzaTopping)
-    var x = pizzaTopping.reduce(add, 0);
-
-    var total = newPizzaOrder.yourCost();
-    // console.log(this.price);
+    name();
+    console.log(name());
+    var newPizzaOrder = new Pizza(pizzaName, pizzaToppings, pizzaSizeTotal, pizzaToppingTotal)
+    Pizza.topping = pizzaToppingTotal.reduce(add, 0);
+    console.log(Pizza.topping);
+    Pizza.price = newPizzaOrder.yourCost();
+    console.log(Pizza.price);
 
     //Shows your order summary.
     $(".summary").show();
     $(".total").show();
     $("#your-total").append("<br>" + newPizzaOrder.price + "</span></br>");
-    $("#your-pizza").append("</br>" + newPizzaOrder.size + "</br> with: " , + newPizzaOrder.topping + "<br>");
-
+    $("#your-pizza").append("</br>" + Pizza.name + "</br> with: " , + Pizza.toppings + "<br>");
   });
 });
